360d7a4a28cc673cf3399882142cab304cfaf7c3
Exhause <figushki20@mail.ru>
2021-11-27 19:48:41 +0300

    Added second homework

---
+++ simple-calculator/Assets/js/script.js
@@ -0,0 +1,39 @@
+let s = '';
+const myInput = document.querySelector("input");
+function update()
+{
+    myInput.value = s;
+}
+
+function clearS()
+{
+    s = '';
+    update();
+}
+
+function clearC()
+{
+    s = s.substring(0,s.length-1);
+    update();
+}
+
+function read( a )
+{
+    if(a=='p')
+        s = s+Math.PI;
+    else
+        s = s+a;
+    update();
+}
+
+function process()
+{
+    try{
+        s = eval(s);
+        update();
+    }
+    catch{
+        s = "";
+        myInput.value = "Wrong expression!";
+    }
+}
